81722a5a6ca15a40e88dbe1c8bb83b580ea5010a
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-06 16:02:21 +0600

    Add files via upload

---
+++ first_program.cpp
@@ -0,0 +1,13 @@
+﻿// first_program.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    cout << "Сергей" << endl << "31" << endl;
+    
+    return 0;
+}
+
